5056aa80757c1291c74cf5002d1bf65b131f523b
Eswari1607 <eswarikamala1607@gmail.com>
2024-12-29 12:16:30 +0530

    Update UserBookingModal.jsx

--- src/components/UserBookingModal.jsx
+++ src/components/UserBookingModal.jsx
@@ -206,8 +206,11 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 800,
+  width: "80%",
+  height: "80%",
   bgcolor: "background.paper",
   boxShadow: 24,
-  p: 4,
+  p: 3,
+  overflowY: "auto",
+  borderRadius: 2,
 };
